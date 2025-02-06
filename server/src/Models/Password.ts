f293897d56ff8a5d0f29bad740f897092bbe9c00
OlafVanHuusen <lennard.clicque@fau.de>
2025-02-06 01:24:08 +0100

    implemented Password Value Type

---
+++ server/src/Models/Password.ts
@@ -0,0 +1,66 @@
+export enum PasswordStrength {
+    VeryWeak = 1,
+    Weak = 2,
+    Medium = 3,
+    Strong = 4,
+    VeryStrong = 5
+}
+
+export class Password {
+
+    private constructor(private readonly passwordValue: string, private readonly strength: PasswordStrength) {}
+    
+    public static create(passwordValue: string): Password {
+        if (!passwordValue || passwordValue === undefined) {
+            throw new Error("Password value cannot be empty");
+        }
+        return new Password(passwordValue, Password.calculatePasswordStrength(passwordValue));
+    }
+
+    public getValue(): string {
+        return this.passwordValue;
+    }
+
+    public getStrength(): PasswordStrength {
+        return this.strength;
+    }
+
+    public toString(): string {
+        return this.passwordValue;
+    }
+
+    private static calculatePasswordStrength(value: string): PasswordStrength {
+        // Check if the password is very weak
+        if (value.length < 8) {
+            return PasswordStrength.VeryWeak;
+        }
+
+        var baseValue = 1;
+        if (Password.containsLowerAndUpperCase(value)) {
+            baseValue++;
+        }
+        if (Password.containsNumber(value)) {
+            baseValue++;
+        }
+        if (Password.containsSpecialCharacter(value)) {
+            baseValue++;
+        }
+        if (value.length >= 12) {
+            baseValue++;
+        }
+
+        return baseValue as PasswordStrength;
+    }
+
+    private static containsLowerAndUpperCase(value: string): boolean {
+        return value !== value.toLowerCase() && value !== value.toUpperCase();
+    }    
+
+    private static containsNumber(value: string): boolean {
+        return /\d/.test(value);
+    }
+
+    private static containsSpecialCharacter(value: string): boolean {
+        return /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>~\/?]+/.test(value);
+    }
+}
